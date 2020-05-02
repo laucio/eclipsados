d2d5ca37a49bf5c3bb57acfad9841a21b8c08f4c
Lautaro Cardarelli <lautaro.cardarelli@ds.d3sistemas.com.ar>
2020-05-02 16:39:13 -0300

    agregado clase Connection

---
+++ src/source/Connection.java
@@ -0,0 +1,22 @@
+package source;
+
+public class Connection {
+	
+	private String direction;
+	private Location location;
+
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	
+}
