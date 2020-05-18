33692fdcdcad4bdd193795b1700932308a6ee310
cordobafabian <cordobafabian@live.com.ar>
2020-05-18 14:25:35 -0300

    Tests Control So far...5 fail tests to be fixed

--- src/juegotest/AdventureTest.java
+++ src/juegotest/AdventureTest.java
@@ -222,38 +222,112 @@ public class AdventureTest {
 		action = new Action("usar","rociador con cerveza de raiz", "item","pirata fantasma", "npcs");
 		actuals = jugador.switchearAction(action);
 		
-		//Ir a taberna	
+		//Hablar con npcs
 		action = new Action("hablar",null, null,"pirata fantasma", "npcs");
 		actuals = jugador.switchearAction(action);
 		assertEquals(expected, actuals);
 		
 	}
 	
+	
 	@Test
-	public void usarItemSobrePirata() {
+	public void UsarRociadorConNpcQueNoEstaEnPlace() {
+		expected = "No ha servido de nada.";	
+		
+		jugador.tomarItem("rociador con cerveza de raiz");
+		//Eliminar obstaculo
+		action = new Action("usar","rociador con cerveza de raiz", "item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		
+		//intenar volver a usar rociador sobre fantasma
+		action = new Action("usar","rociador con cerveza de raiz", "item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		assertEquals(expected, actuals);
+		
+		//FUNCIONA
+		
+	}
+	
+	
+	
+	@Test
+	public void usarRociadorSobrePirata_TengoEnInventario_SePuedeUsar() {
 		expected = "Me encanta la cerveza de raiz! El pirata fantasma se veia entusiasmado por tu ofrecimiento... sin embargo, cuando lo rociaste comenzo a desintegrarse. La mitad de arriba de su cuerpo se desvanecio, y las piernas inmediatamente echaron a correr.";
 		jugador.tomarItem("rociador con cerveza de raiz");
 		action = new Action("usar","rociador con cerveza de raiz", "item","pirata fantasma", "npcs");
 		actuals = jugador.switchearAction(action);
-		
 		assertEquals(expected,actuals);
+		
+		//FUNCIONA
 	}
 	
+	@Test
+	public void usarEspejoSobrePirata_TengoEnInventario_NoSePuedeUsar() {
+		expected = "No ha servido de nada.";
+		jugador.tomarItem("espejo");
+		action = new Action("usar","espejo", "item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		System.out.println(actuals);
+		assertEquals(expected,actuals);
+		
+		//FALLA... SI SE TIENE EN INVENTARIO. DEBERIA DECIR QUE NO SE PUEDE REALIZAR ESA ACCION O QUE NO TIENE EFECTO
+		
+	}
 	
 	@Test
-	public void usarItemSobreItem() {
+	public void usarBarretaSobrePirata_EstaEnInventario_NoSePuedeUsar() {
+		expected = "No ha servido de nada.";
+		jugador.tomarItem("barreta");
+		action = new Action("usar","barreta","item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		System.out.println(actuals);
+		assertEquals(expected,actuals);
+		
+		//FALLA
+		//Dice que no tiene objeto, pero si lo tiene... 
+		//el problema es que no puede usar este objeto sobre un npcs
+	}
+	
+	@Test
+	public void usarBarretaSobrePirata_NoEstaEnInventario_NoSePuedeUsar() {
+		expected = "No tienes ese objeto.";
+
+		action = new Action("usar","barreta","item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		assertEquals(expected,actuals);
+		
+		//FUNCIONA
+		
+	}
+	
+	@Test
+	public void usarRociadorSobrePirata_EstaEnInventario_SePuedeUsar() {
+		expected = "Me encanta la cerveza de raiz! El pirata fantasma se veia entusiasmado por tu ofrecimiento... sin embargo, cuando lo rociaste comenzo a desintegrarse. La mitad de arriba de su cuerpo se desvanecio, y las piernas inmediatamente echaron a correr.";
+		jugador.tomarItem("rociador con cerveza de raiz");
+		action = new Action("usar","rociador con cerveza de raiz","item","pirata fantasma", "npcs");
+		actuals = jugador.switchearAction(action);
+		//System.out.println(actuals);
+		assertEquals(expected,actuals);
+		
+		//FUNCIONA
+		
+	}
+	
+	@Test
+	public void usarBarretaSobreEspejo_TengoSoloBarreta_SePuedeUsar() {
 		expected = "El espejo se ha roto";
 		jugador.tomarItem("barreta");
 		action = new Action("usar","barreta", "item","espejo", "item");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
-		
 		assertEquals(expected,actuals);
+		
+		//FUNCIONA
 	}
 	
 	@Test
 	public void verAfterTriggerDeItem() {
 		//Luego del trigger, el espejo ya no deberia esar en el muelle
+		//REMOVE DE PLACE
 		
 		expected = "Estas en un muelle. En el suelo hay: un rociador con cerveza de raiz. Hay un pirata fantasma. Al sur hay una taberna.";
 		jugador.tomarItem("barreta");
@@ -265,20 +339,32 @@ public class AdventureTest {
 		
 		assertEquals(expected,actuals);//si el trigger de espejo tuviera remove en lugar de default
 		//assertNotEquals(expected,actuals);//si en trigger self de espejo hay default
+		
+		//FUNCIONA
 	}
 	
 	
 	@Test
-	public void usarItemSobreSelf() {
+	public void usarEspejoSobreSelf_LoTengoEnInventario() {
 		expected = "Te ves muy bello hoy";
 		jugador.tomarItem("espejo");
 		action = new Action("usar","espejo", "item","self", "self");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
 		assertEquals(expected,actuals);
 		//Cuando se quiera usar un objeto sobre si mismo, en la action se usa self para target y effect_over 
 		//si se quiere q el item dispare un trigger, debe tener en su array de targets "self" y su trigger debe tener: type="item", 
 		
+		//FUNCIONA
+	}
+	
+	@Test
+	public void usarEspejoSobreSelf_NoLoTengoEnInventario() {
+		expected = "No tienes ese objeto.";
+		action = new Action("usar","espejo", "item","self", "self");
+		actuals = jugador.switchearAction(action);
+		assertEquals(expected,actuals);
+		
+		//FUNCIONA
 	}
 	
 	@Test
@@ -292,7 +378,33 @@ public class AdventureTest {
 		//Cuando se quiera usar un objeto sobre si mismo, en la action se usa self para target y effect_over 
 		//si se quiere q el item dispare un trigger, debe tener en su array de targets "self" y su trigger debe tener: type="item", 
 		
+		
+		//FUNCIONA
 	}
+	
+	@Test
+	public void usarBarretaSobreSelf_LoTengoEnInventario_NoSePuedeUsar() {
+		expected = "No ha servido de nada.";
+		jugador.tomarItem("barreta");
+		action = new Action("usar","barreta", "item","self", "self");
+		actuals = jugador.switchearAction(action);
+		assertNotEquals(expected,actuals);
+		
+		//FUNCIONA
+	}
+	
+	@Test
+	public void usarBarretaSobreSelf_NoLoTengoEnInventario_NoSePuedeUsar() {
+		expected = "No tienes ese objeto.";
+		action = new Action("usar","barreta", "item","self", "self");
+		actuals = jugador.switchearAction(action);
+		System.out.println(actuals);
+		assertEquals(expected,actuals);
+		
+		//FUNCIONA
+	}
+	
+	
 	
 	@Test
 	public void usarBarretaSobreRociador() {
@@ -302,7 +414,6 @@ public class AdventureTest {
 		jugador.tomarItem("barreta");
 		action = new Action("usar","barreta","item","rociador con cerveza de raiz", "item");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
 		assertEquals(expected,actuals);
 		
 		//FUNCIONA
@@ -320,34 +431,25 @@ public class AdventureTest {
 		System.out.println(actuals);
 		assertEquals(expected,actuals);
 		
-		//FALLA: CUANDO SE TIENE EL OBJETO Q REALIZA ACCION EN INVENTARIO Y EL Q LA RECIBE ESTA EN EL PLACE, NO DICE QUE NO ENCUENTRA EL SEGUNDO
+		//FALLA: CUANDO SE TIENEN EN INVENTARIO EL OBJETO Q REALIZA ACCION Y EL Q LA RECIBE, DICE QUE NO ENCUENTRA EL SEGUNDO
+		//CUANDO BUSCA EL SEGUNDO, TAMBIEN DEBE BUSCARLO EN INVENTARIO... SI HAY REMOVE, TAMBIN DEBE QUITARLO DE INVENTARIO
+		//DEBERIA FUNCIONAR
 	}
 	
 	
-	
 	@Test
-	public void usarBarretaSobreSelf() {
-		expected = "Se ha abollado la lata";
-		jugador.tomarItem("barreta");
-		action = new Action("usar","barreta","item","self", "self");
-		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
-		assertEquals(expected,actuals);
-		
-		//REVISAR--- DEVUELVE "No tienes el objeto, cuando ya tienes la barreta
-	}
-	
-	@Test
-	public void usarEspejoSobreBarreta_AmbosEnInventario() {
+	public void usarEspejoSobreBarreta_AmbosEnInventario_NoSePuedeUsar() {
 		expected = "Se ha abollado la lata";
 		jugador.tomarItem("espejo");
 		jugador.tomarItem("barreta");
 		action = new Action("usar","espejo","item","barreta", "item");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
+		System.out.println(actuals);
 		assertEquals(expected,actuals);
 		
+		//FALLA
 		//REVISAR--- DEVUELVE "No tienes ese objeto" y en realidad tiene ambos
+		//Deberia decir que no puede realizar eso o que la accion no tiene efecto
 	}
 
 	@Test
@@ -355,18 +457,20 @@ public class AdventureTest {
 		expected = "No tienes ese objeto.";
 		action = new Action("usar","espejo","item","barreta", "item");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
 		assertEquals(expected,actuals);
+		
+		//FUNCIONA
 	}
 	
 	@Test
 	public void usarBarretaSobreEspejo_SoloTieneEspejoEnInventario_SePuedeUsar() {
-		expected = "No tienes ese objeto";
+		expected = "No tienes ese objeto.";
 		jugador.tomarItem("espejo");
 		action = new Action("usar","barreta","item","espejo", "item");
 		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
 		assertEquals(expected,actuals);
+		
+		//FUNCIONA
 	}
 	
 	@Test
@@ -375,15 +479,14 @@ public class AdventureTest {
 		jugador.tomarItem("barreta");
 		action = new Action("usar","espejo","item","barreta", "item");
 		actuals = jugador.switchearAction(action);
-		System.out.println(actuals);
 		assertEquals(expected,actuals);
 		
-		//Da "Bien"... quiere usar algo que no tiene
+		//FUNCIONA
 	}
 	
 	@Test
 	public void usarEspejoSobreBarreta_TieneAmbosEnInventario_NoSePuedeUsar() {
-		expected = "No tienes ese objeto.";
+		expected = "No ha servido de nada.";
 		jugador.tomarItem("barreta");
 		jugador.tomarItem("espejo");
 		action = new Action("usar","espejo","item","barreta", "item");
@@ -391,64 +494,10 @@ public class AdventureTest {
 		//System.out.println(actuals);
 		assertEquals(expected,actuals);
 		
-		//Revisar...Deberia fallar...Tiene ambos... pero deberia decir que no puede usar espejo sobre barreta
+		//FALLA
+		//Dice que no tenemos el objeto (que queremos usar, primero)
+		//Tiene ambos... pero deberia decir que no puede usar espejo sobre barreta
 	}
-	
-	@Test
-	public void usarEspejoSobrePirata_EstaEnInventario_NoSePuedeUsar() {
-		expected = "No tienes ese objeto.";
-		jugador.tomarItem("espejo");
-		action = new Action("usar","espejo","item","pirata fantasma", "npcs");
-		actuals = jugador.switchearAction(action);
-		System.out.println(actuals);
-		assertEquals(expected,actuals);
-		
-		//REVISAR
-		//Dice que no tiene objeto, pero si lo tiene... el problema es que no puede usar el objeto sobre un npcs
-	}
-	
-	@Test
-	public void usarBarretaSobrePirata_EstaEnInventario_NoSePuedeUsar() {
-		expected = "No tienes ese objeto.";
-		jugador.tomarItem("barreta");
-		action = new Action("usar","barreta","item","pirata fantasma", "npcs");
-		actuals = jugador.switchearAction(action);
-		System.out.println(actuals);
-		assertEquals(expected,actuals);
-		
-		//REVISAR, FALLA
-		//Dice que no tiene objeto, pero si lo tiene... el problema es que no puede usar el objeto sobre un npcs
-	}
-	
-	
-	
-	@Test
-	public void usarRociadorSobrePirata_EstaEnInventario_SePuedeUsar() {
-		expected = "Me encanta la cerveza de raiz! El pirata fantasma se veia entusiasmado por tu ofrecimiento... sin embargo, cuando lo rociaste comenzo a desintegrarse. La mitad de arriba de su cuerpo se desvanecio, y las piernas inmediatamente echaron a correr.";
-		jugador.tomarItem("rociador con cerveza de raiz");
-		action = new Action("usar","rociador con cerveza de raiz","item","pirata fantasma", "npcs");
-		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
-		assertEquals(expected,actuals);
-		
-		//FUNCIONA
-		
-	}
-
-	@Test
-	public void usarBarretaSobrePirata_NoEstaEnInventario_NoSePuedeUsar() {
-		expected = "No tienes ese objeto.";
-
-		action = new Action("usar","barreta","item","pirata fantasma", "npcs");
-		actuals = jugador.switchearAction(action);
-		//System.out.println(actuals);
-		assertEquals(expected,actuals);
-		
-		//FUNCIONA... al vez deberia aclarar que es algo que no se puede hacer
-		
-	}
-	
-	
 	
 	
 	/*
