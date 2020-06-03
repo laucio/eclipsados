179384513cd8b28a6cffbee2b039dcc3ccad91aa
cordobafabian <cordobafabian@live.com.ar>
2020-06-03 20:27:23 -0300

    Test push

--- src/traductor_de_comandos/CommandTranslator.java
+++ src/traductor_de_comandos/CommandTranslator.java
@@ -3,6 +3,6 @@ package traductor_de_comandos;
 import juego_de_aventura.*;
 
 public interface CommandTranslator {
-	
+	//Patron
 	public abstract void translateCommand(String command, Action action, Game game);
 }
