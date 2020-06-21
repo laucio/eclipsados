a071cd44427cc175249907e521d9cfa18f86cadd
Jose Ignacio Lopez <ilopez@seidoranalytics.com>
2020-06-20 22:07:15 -0300

    living terminado

--- src/juego_de_aventura/FileManager.java
+++ src/juego_de_aventura/FileManager.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
 
@@ -55,5 +56,32 @@ public class FileManager {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(AdventureFilePath));
 		bw.write(jsonAdventure);
 		bw.close();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			
+			// ESPEJO
+			Adventure adventure = cargarArchivo("jsonLocal.json");
+			/*ArrayList<String> actions = new ArrayList<String>();
+			ArrayList<String> effects_over = new ArrayList<String>();
+			ArrayList<String> targets = new ArrayList<String>();
+			ArrayList<Trigger> triggers = new ArrayList<Trigger>();
+			actions.add("usar");
+			effects_over.add("self");
+			targets.add("self");
+			triggers.add(new Trigger("item","ventana","Parece que se avecina una tormenta","default"));
+			adventure.getItems().add(new Item("ventana","female","singular",actions,effects_over,targets,triggers));*/
+			adventure.getLocation("living").getConnections().get(1).setLocation("camino");
+			
+			
+			
+			saveAdventureProgress(adventure);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
