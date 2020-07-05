189b6f68b0165133631c8650d6728c61381f6826
Jose Ignacio Lopez <ilopez@seidoranalytics.com>
2020-07-05 17:22:05 -0300

    terminado el danio, queda armar items que curen vida

--- src/test/nueva/aventura/TestIntegrales.java
+++ src/test/nueva/aventura/TestIntegrales.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import juego_de_aventura.*;
 
-
 public class TestIntegrales {
 	String path;
 	Game game;
@@ -21,16 +20,16 @@ public class TestIntegrales {
 		path = "aventura_prueba.json";
 		game = new Game(path);
 	}
-	
+
 	///////////////// NUEVA BIENVENIDA ////////////////////////////////////
-	
+
 	@Test
 	public void RecibirBienvenida() {
 		expected = "Te encuentras en una cabania. Luego de una larga siesta, te levantas con ganas de ir a un bar.";
 		actual = game.getWelcome();
 		assertEquals(expected, actual);
 	}
-	
+
 ///////////////// CURRENT LOCATION CORRECTA ////////////////////////////////////
 
 	@Test
@@ -39,28 +38,28 @@ public class TestIntegrales {
 		actual = game.makePlayerLookAround();
 		assertEquals(expected, actual);
 	}
-	
+
 	/////////// TOMAR NUEVO ITEM EN JSON ///////////////////////////
-	
+
 	@Test
 	public void TomarSanguche() {
 		expected = "En tu inventario hay un sanguche de atun.";
-		Action action = new Action("tomar","sanguche de atun","item",null,null);
+		Action action = new Action("tomar", "sanguche de atun", "item", null, null);
 		game.makePlayerTakeItem(action);
 		actual = game.makePlayerWatchInventory();
 		assertEquals(expected, actual);
 	}
-	
+
 	//////////////// COMER SANGUCHE ///////////////
-	
+
 	@Test
 	public void ComerSanguche() {
 		expected = "Mmmm rikoooo, ahora tengo mas energia.";
-		Action action = new Action("tomar","sanguche de atun","item",null,null);
+		Action action = new Action("tomar", "sanguche de atun", "item", null, null);
 		game.makePlayerTakeItem(action);
-		action = new Action("usar","sanguche de atun","item","self","self");
+		action = new Action("usar", "sanguche de atun", "item", "self", "self");
 		actual = game.makePlayerUseItem(action);
 		assertEquals(expected, actual);
 	}
-	
+
 }
